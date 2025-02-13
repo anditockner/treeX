48da2c8e8cf9f95ba515568785669054a270447c
AndiT <andreas.tockner@boku.ac.at>
2025-02-13 17:28:26 +0100

    include cmd version for treeX

---
+++ WORKFLOW_treeX_cmd.R
@@ -0,0 +1,106 @@
+time1 <- Sys.time()
+# devtools::install_git("https://github.com/anditockner/treeX")
+
+# arguments for cmd run of this script:
+# [1] path to .las or .laz input ground-based point cloud
+# [2] path to configuration file (config.R) defining hyper-parameters
+# [3] (optional) path for saving output files
+# [4] (optional) new name string for output files (fileFinder)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 0) {
+  print("Please specify .las or .laz file to be processed!")
+  stop("Requires command line argument.")
+}
+
+# [1] input point cloud
+LASfile <- args[1]
+if (!file.exists(LASfile)) {
+  print(paste0("File ", LASfile, " not found... Terminating."))
+  stop("Invalid LASfile specified.")
+}
+
+# [2] config.R
+if (length(args) >= 2) {
+  if (!file.exists(args[2])) {
+    print(paste0("Config file ", args[2], " not found... Terminating."))
+    stop("Invalid config-file specified.")
+  }
+  source(args[2])
+}
+
+# [3] directory
+dirPath <- dirname(LASfile) # files and folder structure will be created
+if (length(args) >= 3) {
+  dirPath <- args[3]
+}
+
+# [3] name
+fileFinder <- basename(LASfile)
+if (length(args) >= 4) {
+  fileFinder <- args[4]
+}
+
+cat("Loading treeX package... ")
+so <- tryCatch(find.package("treeX"),
+  error = function(error_condition) {
+    cat("(installing from github)")
+    devtools::install_github(
+      "https://github.com/anditockner/treeX",
+      upgrade = "never"
+    )
+  }
+)
+suppressPackageStartupMessages(library("treeX"))
+cat("done!\n")
+
+if (!dir.exists(dirPath)) dir.create(dirPath)
+setwd(dirPath) # all the files and folder structure will be created here
+cat("Working directory is:", dirPath, "\n")
+
+cat("Starting treeX workflow\n")
+
+cat(paste0("LASfile = ", LASfile), "\n")
+cat(paste0("fileFinder = ", fileFinder), "\n")
+cat("\n\n\n\n")
+
+try(extractVegetation(LASfile, fileFinder,
+  selector = selector,
+  clip.radius = clip.radius, 
+  clip.trajectory.distance = clip.trajectory.distance,
+  exportSlice.upperLimit = exportSlice.upperLimit,
+  exportSlice.lowerLimit = exportSlice.lowerLimit
+))
+
+try(clustSplit(
+  fileFinder = fileFinder, 
+  filterINT = filterINT,
+  nr_cores = nr_cores_tree_detection,
+  retainPointClouds = T
+))
+
+try(crownFeel(fileFinder,
+  voxelSize = voxelSize, 
+  limitStems = limitStems,
+  limitShare = limitShare, 
+  zScale = zScale,
+  selector = selector,
+  tileClipping = 2, 
+  retainPointClouds = F
+))
+
+#
+# try(computeTreeParams(fileFinder, voxelSize = voxelSize,
+#                      limitShare = limitShare, zScale = zScale, nr_cores = 2,
+#                      writeLAZ = F, retainPointClouds = F, crownParameters = F))
+#
+
+{
+  time2 <- Sys.time()
+  timeDiff <- round(time2 - time1, 2)
+  cat("\n\n\nEnding script on", format(Sys.time()), "\n")
+  cat("All jobs done in a ")
+  print.difftime(timeDiff)
+  cat("\n\n\n")
+}
