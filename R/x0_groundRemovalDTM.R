01da203936715408a264f4190e4187821b800111
AndiT <andreas.tockner@boku.ac.at>
2025-03-20 16:43:42 +0100

    expanding intensity to 2^16

--- R/x0_groundRemovalDTM.R
+++ R/x0_groundRemovalDTM.R
@@ -155,8 +155,9 @@ extractVegetation <- function(LASfile, fileFinder, groundMergeCut = 0, ipad = FA
     big@data$Intensity <- 65535L
 #    writeLAS(big, "D:/nowlas.laz")
   }
-  if(max(big@data$Intensity) <= 256){
-    big@data$Intensity <- as.integer((big@data$Intensity / 255L) * 65535)
+  if(max(big@data$Intensity) <= 256L){
+    cat("Expanding intensity from 255 to 65535...\n")
+    big@data$Intensity <- integer((big@data$Intensity / 255L) * 65535)
     #    writeLAS(big, "D:/nowlas.laz")
   }
 
